92cca1cf632ffc691117ced753bf1c7a0b3163a9
friedchickenblob <jck2@ualberta.ca>
2026-02-21 16:13:59 -0700

    fix db url

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./test.db"
+# DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
--- backend/app/db.py
+++ backend/app/db.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from .models import Base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # dev only
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # dev only
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
